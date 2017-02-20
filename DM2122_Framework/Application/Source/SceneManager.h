2bfb60034d0d24969171da632ed3cf229380f5fe
Jolyn Wong <shiroyami08@gmail.com>
2017-02-20 09:21:28 +0800

    added scene manager h and cpp

--- DM2122_Framework/Application/Source/SceneManager.h
+++ DM2122_Framework/Application/Source/SceneManager.h
@@ -1,39 +1,32 @@
 #ifndef SCENEMANAGER_H
 #define SCENEMANAGER_H
-
-#include <iostream>
-#include <vector>
+#include "Application.h"
 #include "Scene.h"
+#include "Space.h"
 #include "PlanetOne.h"
 #include "PlanetTwo.h"
 #include "PlanetThree.h"
 #include "PlanetFour.h"
-#include "Space.h"
 #include "InternalSpaceShip.h"
-
-using namespace std;
+#include "StudioProj.h"
 
 class SceneManager
 {
-	//vector<int> StoreScene;//storing of scene
-	//int currSceneID;
-	//int nextSceneID;
+	int storeScenes[10];//store scenes into array
+	int currSceneID;
+	int nextSceneID;
 
 public:
 	SceneManager();
 	~SceneManager();
 
-	Scene *scene;
-	Scene *scene1;//internal of ship
-	Scene *scene2;//space
-	Scene *scene3;//jupiter
-	Scene *scene4;//saturn
-	Scene *scene5;//blue planet
-	Scene *scene6;//mars
+	void AddScene(Scene *scene);//add pointer to a scene
+	void SetNextScene(int SceneID);
+	void Update();
 
-	void AddScene();//add scene
-	void SetNextScene(int sceneID);//setting next scene using the id
-	void Update();//run what Application::Run while loop is doing
 };
+
+
+
 
 #endif
