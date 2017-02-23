adb84a341b55d60c62b362fc48c1678f45906032
hongsheng322 <hongsheng322@gmail.com>
2017-02-24 00:02:52 +0800

    inventory system and maximum capacity

--- DM2122_Framework/Application/Source/Inventory.cpp
+++ DM2122_Framework/Application/Source/Inventory.cpp
@@ -11,6 +11,12 @@ Inventory::~Inventory()
 
 }
 
+int Inventory::storage[2][10]
+{
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+};
+
 void Inventory::addCommon()
 {
 	common++;
@@ -27,4 +33,73 @@ void Inventory::addEpic()
 {
 	epic++;
 	std::cout << "epic:" << epic << std::endl;
+}
+
+void Inventory::assignItem(int itemID)
+{
+	ID = itemID;
+	empty = true;
+
+	if (ID == 1)
+	{
+		for (int i = 0; i < 10 && empty == true; i++)
+		{
+			if (storage[0][i] == 1 && storage[1][i] < 10)
+			{
+				storage[1][i] += 1;
+				empty = false;
+			}
+			else if (storage[0][i] == 0)
+			{
+				storage[0][i] = 1;
+				storage[1][i] = 1;
+				empty = false;
+			}
+		}
+	}
+	if (ID == 2)
+	{
+		for (int i = 0; i < 10 && empty == true; i++)
+		{
+			if (storage[0][i] == 2 && storage[1][i] < 10)
+			{
+				storage[1][i] += 1;
+				empty = false;
+			}
+			else if (storage[0][i] == 0)
+			{
+				storage[0][i] = 2;
+				storage[1][i] = 1;
+				empty = false;
+			}
+		}
+	}
+	if (ID == 3)
+	{
+		for (int i = 0; i < 10 && empty == true; i++)
+		{
+			if (storage[0][i] == 3 && storage[1][i] < 10)
+			{
+				storage[1][i] += 1;
+				empty = false;
+			}
+			else if (storage[0][i] == 0)
+			{
+				storage[0][i] = 3;
+				storage[1][i] = 1;
+				empty = false;
+			}
+		}
+	}
+
+	std::cout << "storage 0 ID is : " << storage[0][0] << "    count is:" << storage[1][0] << std::endl;
+	std::cout << "storage 1 ID is : " << storage[0][1] << "    count is:" << storage[1][1] << std::endl;
+	std::cout << "storage 2 ID is : " << storage[0][2] << "    count is:" << storage[1][2] << std::endl;
+	std::cout << "storage 3 ID is : " << storage[0][3] << "    count is:" << storage[1][3] << std::endl;
+	std::cout << "storage 4 ID is : " << storage[0][4] << "    count is:" << storage[1][4] << std::endl;
+	std::cout << "storage 5 ID is : " << storage[0][5] << "    count is:" << storage[1][5] << std::endl;
+	std::cout << "storage 6 ID is : " << storage[0][6] << "    count is:" << storage[1][6] << std::endl;
+	std::cout << "storage 7 ID is : " << storage[0][7] << "    count is:" << storage[1][7] << std::endl;
+	std::cout << "storage 8 ID is : " << storage[0][8] << "    count is:" << storage[1][8] << std::endl;
+	std::cout << "storage 9 ID is : " << storage[0][9] << "    count is:" << storage[1][9] << std::endl;
 }
--- DM2122_Framework/Application/Source/Inventory.h
+++ DM2122_Framework/Application/Source/Inventory.h
@@ -1,23 +1,30 @@
 #ifndef INVENTORY_H
 #define INVENTORY_H
+
 #include <iostream>
-#include <string>
+
 using namespace std;
 
 class Inventory
 {
-	public:
-		Inventory();
-		~Inventory();
+public:
+	Inventory();
+	~Inventory();
 
-		void addCommon();
-		void addRare();
-		void addEpic();
+	void addCommon();
+	void addRare();
+	void addEpic();
+	void assignItem(int itemID);
+	static int storage[2][10];
 
-	private:
-		int common;
-		int rare;
-		int epic;
+private:
+	int common;
+	int rare;
+	int epic;
+	bool empty;
+	bool stop;
+	int ID;
+
 };
 
 #endif
